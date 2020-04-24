a7bceee40bb213ea37723d629b94cb78b3c43dc1
david <thedavidomole@gmail.com>
2020-04-24 17:35:40 +0100

    Style: Bootstrap header section

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $(".featured-slider").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
@@ -11,24 +11,24 @@ $(document).ready(function() {
         breakpoint: 768,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
-        }
+          slidesToScroll: 2,
+        },
       },
       {
         breakpoint: 670,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
         },
         breakpoint: 580,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
+          slidesToScroll: 1,
+        },
+      },
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
-    ]
+    ],
   });
 });
