67533d41f213bf6f2deb4072c1b8d3d98a23e6a1
david <thedavidomole@gmail.com>
2020-05-11 17:31:30 +0100

    -a

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $(".featured-slider").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 3000,
     nextArrow: $(".next"),
     prevArrow: $(".prev"),
